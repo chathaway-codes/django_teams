0bf00393d2284310e18d60e22f645088c3052300
Charles H <chathaway@logrit.com>
2016-03-16 10:26:12 -0400

    Removed unused code, now works with Django 1.9.4

--- django_teams/models.py
+++ django_teams/models.py
@@ -9,7 +9,6 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models.fields.related import SingleRelatedObjectDescriptor
 
 # Using the user: user = models.ForeignKey(settings.AUTH_USER_MODEL)
 CurrentUser = None
@@ -144,45 +143,3 @@ class Ownership(models.Model):
         if res[1]:
             res[0].save()
 
-def override_manager(model):
-    def f(self):
-      import django_teams.models
-      #sys.stdout.write("In overridemanager for "+repr(self.model)+"\n")
-      #sys.stdout.write(repr(dir(self)))
-      #sys.stdout.write("\n")
-      #sys.stdout.flush()
-      if django_teams.models.CurrentUser == None:
-          return QuerySet(model=self.model, using=self._db)
-      else:
-          # Get a list of objects on this model that the user has access too
-          content_type = ContentType.objects.get_for_model(self.model)
-          pk_list = None
-          if django_teams.models.CurrentTeam != None:
-              # If they are only invited to current team, raise an error
-              if TeamStatus.objects.get(team=django_teams.models.CurrentTeam, user=django_teams.models.CurrentUser).role < 10:
-                  raise ObjectDoesNotExist()
-              pk_list = Ownership.objects.values_list("object_id", flat=True).filter(content_type=content_type, team=django_teams.models.CurrentTeam)
-          else:
-              pk_list = Ownership.objects.values_list("object_id", flat=True).filter(content_type=content_type, team__in=django_teams.models.CurrentUser.team_set.filter(teamstatus__role__gte=10))
-          return QuerySet(model=self.model, using=self._db).filter(pk__in=pk_list)
-    #sys.stdout.write("Overriding "+repr(model)+"\n")
-    #sys.stdout.flush()
-    if inspect.isclass(model) and issubclass(model, models.Model):
-        model.objects._get_queryset = model.objects.get_queryset
-        model.objects.get_queryset = types.MethodType(f, model.objects)
-    elif type(model) is not SingleRelatedObjectDescriptor:
-        try:
-            model.related_manager_cls._get_queryset = model.related_manager_cls.get_queryset
-            model.related_manager_cls.get_queryset = types.MethodType(f, model.related_manager_cls)
-        except:
-            pass
-
-def revert_manager(model):
-    if hasattr(model.objects, '_get_queryset'):
-        if issubclass(model, models.Model):
-            model.objects.get_queryset = model.objects._get_queryset
-        elif type(model) is not SingleRelatedObjectDescriptor:
-            try:
-                model.related_manager_cls.get_queryset = model.related_manager_cls._get_queryset
-            except:
-                pass
